d535e7d9de98abb7fa82a350e99f541cfda623d2
Taming Linux <yourlinuxteacher@gmail.com>
2024-09-15 12:14:04 +0300

    Update install.sh
    grub issue

--- install.sh
+++ install.sh
@@ -6,14 +6,38 @@ if [ "$(id -u)" -ne "0" ]; then
   exit 1
 fi
 
-ping -c 2 www.archlinux.org
+# Проверяем подключение к сети интернет
+ping -c 1 www.archlinux.org
+ping -c 1 www.google.com
 
+# Настройка времени
+echo "Настраиваем системное время..."
 timedatectl set-ntp true
 timedatectl set-timezone Europe/Moscow
 
-# Размечаем диск =) Добавить возможность смены раздела на любой (или дать выбор)
-echo "Произвожу разметку диска"
-fdisk /dev/sda <<EOF
+# Выбор диска для установки системы
+echo "Список доступных дисков:"
+lsblk
+read -p "Укажите диск для разметки (например, /dev/sda): " DISK
+
+fdisk "$DISK" <<EOF
+g
+n
+1
+
+
+
+Y
+
+
+w
+EOF
+# Erase Disk!!
+dd if=/dev/random of="$DISK" bs=512 bc=100
+
+# Разметка диска
+echo "Произвожу разметку диска $DISK"
+fdisk "$DISK" <<EOF
 g
 n
 1
@@ -43,17 +67,17 @@ w
 EOF
 
 # Форматирование разделов
-echo "Подготовка размеченного пространства"
-mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
-swapon -v /dev/sda2
-mkfs.ext4 /dev/sda3
+echo "Форматирую разделы..."
+mkfs.fat -F32 "${DISK}1"
+mkswap "${DISK}2"
+swapon -v "${DISK}2"
+mkfs.ext4 "${DISK}3"
 
 # Монтирование ФС
 echo "Монтирую разделы.."
-mount /dev/sda3 /mnt
+mount "${DISK}3" /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount "${DISK}1" /mnt/boot
 
 # Устанавливаю системные файлы
 echo "Устанавливаю системные файлы.."
@@ -64,35 +88,43 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # chroot
 echo "Переходим в chroot"
-arch-chroot /mnt /bin/base <<EOF
+arch-chroot /mnt /bin/bash <<EOF
 
   #Установка часового пояса
-  ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
-  hwclock --systohc
-  echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-  echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
-  locale-gen
-  echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  sudo ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+  sudo hwclock --systohc
+  sudo echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+  sudo echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
+  sudo locale-gen
+  sudo echo "LANG=en_US.UTF-8" > /etc/locale.conf
   # Установка hostname и настройка сети
-  echo "archbox" > /etc/hostname
-  echo "127.0.0.1	localhost
+  sudo echo "archbox" > /etc/hostname
+  sudo echo "127.0.0.1	localhost
   ::1	localhost
   127.0.1.1	archbox.localdomain archbox" > /etc/hosts
   
-  useradd -m  taminglinux
-  passwd taminglinux
+  sudo useradd -m  taminglinux
+  sudo echo "Установите пароль для пользователя по умолчанию:"
+  sudo passwd taminglinux
   
-  usermod -aG wheel,audio,video,optical,storage taminglinux
+  sudo usermod -aG wheel,audio,video,optical,storage taminglinux
   
-  pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools dhcpcd
-  # pacman -S --noconfirm iwd
+  sudo pacman -S --noconfirm sudo
+  sudo echo "taminglinux ALL=(ALL) ALL" >> /etc/sudoers
   
-  EDITOR=vim
-  visudo
+  sudo pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools dhcpcd
+   
+  sudo echo "Перед повторным маунтом EFI, я выведу тебе стату по дискам"
+  sudo  df -h
+   
+  sudo mkdir /boot/EFI
+  sudo mount "${DISK}1" /boot/EFI
+
+  sudo grub-install --target=x86_64-efi --bootloader-id=Arch_UEFI --recheck
+  sudo grub-mkconfig -o /boot/grub/grub.cfg
   
-  grub-install --target=x86_64-efi --bootloader-id=Arch_UEFI --recheck
-  grub-mkconfig -o /boot/grub/grub.cfg
-  
+  # Включаем сеть
+  sudo systemctl enable dhcpcd
 EOF
 
 echo "Установка завершена. Перезагрузите систему."
