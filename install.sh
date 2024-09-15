63ad18b1bc9f3da6289af8ec2418d53dac4445ee
Taming Linux <yourlinuxteacher@gmail.com>
2024-09-15 09:37:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-# 1Проверить, что скрипт запущен от рута
+# Проверяем, что скрипт запущен под root
 if [ "$(id -u)" -ne "0" ]; then
-  echo "Скрипт нужно запустить от имени root" 1>&2
+  echo "Запустите скрипт с повышенными привилегиями..." 1>&2
   exit 1
 fi
-
-# 2Установка базовых компонентов
-
-echo "Устанавливаю базовые компоненты.."
-pacman -Syu --noconfirm
-pacman -S --noconfirm base base-devel linux linux-firmware vim
 
 # Размечаем диск =) Добавить возможность смены раздела на любой (или дать выбор)
 echo "Произвожу разметку диска"
 fdisk /dev/sda <<EOF
-o
+g
 n
-p
 1
 
-
 +512M
-t
-ef
+Y
 n
-p
 2
 
++1G
+n
+3
+
 
 t
+1
+1
+t
+2
+19
+t
+2
 20
 w
 EOF
@@ -37,11 +38,13 @@ EOF
 # Форматирование разделов
 echo "Подготовка размеченного пространства"
 mkfs.fat -F32 /dev/sda1
-mkfs.ext4 /dev/sda2
+mkswap /dev/sda2
+swapon -v /dev/sda2
+mkfs.ext4 /dev/sda3
 
 # Монтирование ФС
 echo "Монтирую разделы.."
-mount /dev/sda2 /mnt
+mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
@@ -53,4 +56,20 @@ echo "Генерация файловой системы"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # chroot
+echo "Переходим в chroot"
+arch-chroot /mnt /bin/base <<EOF
 
+  #Установка часового пояса
+  ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+  locale-gen
+  echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  # Установка hostname и настройка сети
+  echo "archbox" > /etc/hostname
+  pacman -S --noconfirm grub
+  grub-install --target=x86_64 /dev/sda
+  grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+echo "Установка завершена. Перезагрузите систему."
+
+exit 0
