822ab9183ba15890044b4089da48c08f28124ea2
Taming Linux <yourlinuxteacher@gmail.com>
2024-09-15 11:22:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ if [ "$(id -u)" -ne "0" ]; then
   exit 1
 fi
 
+ping -c 2 www.archlinux.org
+
+timedatectl set-ntp true
+timedatectl set-timezone Europe/Moscow
+
 # Размечаем диск =) Добавить возможность смены раздела на любой (или дать выбор)
 echo "Произвожу разметку диска"
 fdisk /dev/sda <<EOF
@@ -19,10 +24,12 @@ n
 2
 
 +1G
+Y
 n
 3
 
 
+Y
 t
 1
 1
@@ -50,7 +57,7 @@ mount /dev/sda1 /mnt/boot
 
 # Устанавливаю системные файлы
 echo "Устанавливаю системные файлы.."
-pacstrap /mnt base linux linux-virmware vim
+pacstrap /mnt base linux linux-firmware vim
 
 echo "Генерация файловой системы"
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -61,13 +68,31 @@ arch-chroot /mnt /bin/base <<EOF
 
   #Установка часового пояса
   ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+  hwclock --systohc
+  echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+  echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
   locale-gen
   echo "LANG=en_US.UTF-8" > /etc/locale.conf
   # Установка hostname и настройка сети
   echo "archbox" > /etc/hostname
-  pacman -S --noconfirm grub
-  grub-install --target=x86_64 /dev/sda
+  echo "127.0.0.1	localhost
+  ::1	localhost
+  127.0.1.1	archbox.localdomain archbox" > /etc/hosts
+  
+  useradd -m  taminglinux
+  passwd taminglinux
+  
+  usermod -aG wheel,audio,video,optical,storage taminglinux
+  
+  pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools dhcpcd
+  # pacman -S --noconfirm iwd
+  
+  EDITOR=vim
+  visudo
+  
+  grub-install --target=x86_64-efi --bootloader-id=Arch_UEFI --recheck
   grub-mkconfig -o /boot/grub/grub.cfg
+  
 EOF
 
 echo "Установка завершена. Перезагрузите систему."
